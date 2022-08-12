92ee8f7c2b19af40f18328e17776f4688ca8e6d7
Alejandro Pozo <hap921020@gmail.com>
2022-08-12 10:53:25 -0500

    paginado YA!

--- public/assets/js/paginado.js
+++ public/assets/js/paginado.js
@@ -18,7 +18,7 @@ arrDiv[0].classList.remove('t-inactive')
 arrUl.forEach((a, i) => {
     a.addEventListener('click', (event) => {
         event.stopPropagation()
-        arrDiv[i].classList.toggle('t-inactive')
+        arrDiv[i].classList.remove('t-inactive')
         const arrResto = arrDiv.filter(div => div != arrDiv[i])
         arrResto.forEach(caja => {
             caja.classList.add('t-inactive')
