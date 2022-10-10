c831e98a39a6f9d3accb2bd696f0ce7cf811e385
kpawn92 <hap921020@gmail.com>
2022-10-09 20:20:43 -0400

    update db

--- app/Database/db_almacen_phpMysql.sql
+++ app/Database/db_almacen_phpMysql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 07-10-2022 a las 18:36:33
+-- Tiempo de generación: 10-10-2022 a las 02:18:51
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -38,9 +38,9 @@ CREATE TABLE `op_books_disponibles` (
 --
 
 INSERT INTO `op_books_disponibles` (`id`, `fk_libro`, `c_disponibles`) VALUES
-(29, 52, 143),
-(30, 51, 142),
-(31, 53, 152);
+(29, 52, 142),
+(30, 51, 141),
+(31, 53, 151);
 
 -- --------------------------------------------------------
 
@@ -132,10 +132,9 @@ CREATE TABLE `tb_estudiante` (
 
 INSERT INTO `tb_estudiante` (`id`, `ci`, `nombre`, `lastname`, `nation`, `direccion`, `fk_carrera`, `fk_year_academico`, `fk_brigada`, `status`) VALUES
 (102, '92102047481', 'ALEJANDRO', 'SOSA GOMEZ', 0, 'Cuba-cu-Manzanillo Reparto Cespedez #6', 1, 2, 1, 0),
-(103, '44455686564', 'ALEJANDRO', 'POZO CASTRO', 1, 'Alemania-de-Berlin', 1, 2, 1, 0),
-(104, '23423234553', 'JUAN', 'SUAREZ CESPEDEZ', 1, 'Argentina-ar-Buenos aires', 1, 1, 1, 0),
 (106, '45481215785', 'LEANDRO', 'POZO CASTRO', 0, 'Cuba-cu-Niquero Reparto Vazquez', 3, 1, 1, 0),
-(107, '23234234232', 'JUAN', 'SUAREZ CESPEDEZ', 1, 'Guinea Ecuatorial-gq-Estus', 1, 1, 1, 0);
+(107, '23234234232', 'JUAN', 'SUAREZ CESPEDEZ', 1, 'Guinea Ecuatorial-gq-Estus', 1, 1, 1, 0),
+(108, '22222222222', 'JUANA', 'CASTRO LASTRE', 0, 'Cuba-cu-34 Malecon', 1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -159,7 +158,7 @@ CREATE TABLE `tb_libro` (
 --
 
 INSERT INTO `tb_libro` (`id`, `codigo`, `titulo`, `portada`, `precio`, `autor`, `isbn`, `cantidad`) VALUES
-(51, 'RES-458', 'PHP', '1663818188_3c794c598375d9db95d5.jpeg', '126.35', 'ARTUH GONSAS', '458-ASA458-48', 89),
+(51, 'RES-458', 'Lorem ipsum dolor sit amet, consectetur adipiscing', '1663818188_3c794c598375d9db95d5.jpeg', '126.35', 'ARTUH GONSAS', '458-ASA458-48', 89),
 (52, 'RES-459', 'JS', '1663818265_d6c55dffcad27908ebdc.jpeg', '14.56', 'JUAN SOSA AGUILERA', '458-ASA458-48', 194),
 (53, 'RES-462', 'SQL', '1664663405_e13f7534b5e0fa0e607b.jpg', '136.84', 'ARTUH ERTHREES', '458-ASA458-48', 170);
 
@@ -185,7 +184,9 @@ CREATE TABLE `tb_order` (
 
 INSERT INTO `tb_order` (`id`, `fk_estudiante`, `libros_id`, `pay`, `date_order`, `date_okay`, `condition`) VALUES
 (54, 102, '30,29', '140.91', 1665158160, NULL, 0),
-(55, 102, '30,29,31', '277.75', 1665159292, NULL, 0);
+(55, 102, '30,29,31', '277.75', 1665159292, NULL, 0),
+(56, 108, '30,29', '140.91', 1665358403, NULL, 0),
+(57, 108, '31', '136.84', 1665358408, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -208,10 +209,9 @@ CREATE TABLE `tb_users` (
 INSERT INTO `tb_users` (`id`, `usuario`, `password`, `rol`, `logged`) VALUES
 (1, 'root', '1234', 1, 0),
 (13, 'usuario', '92102047481', 3, 0),
-(14, 'usuario', '44455686564', 3, 0),
-(15, 'usuario', '23423234553', 3, 0),
 (17, 'usuario', '45481215785', 3, 0),
-(18, 'usuario', '23234234232', 3, 0);
+(18, 'usuario', '23234234232', 3, 0),
+(19, 'usuario', '22222222222', 3, 0);
 
 -- --------------------------------------------------------
 
@@ -331,7 +331,7 @@ ALTER TABLE `tb_carrera`
 -- AUTO_INCREMENT de la tabla `tb_estudiante`
 --
 ALTER TABLE `tb_estudiante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_libro`
@@ -343,13 +343,13 @@ ALTER TABLE `tb_libro`
 -- AUTO_INCREMENT de la tabla `tb_order`
 --
 ALTER TABLE `tb_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_users`
 --
 ALTER TABLE `tb_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_year_academico`
